863413508285f4bb0f188b0d9eaf6446d39feff0
olajidecares1 <olahcares@gmail.com>
2026-09-16 17:12:16 +0100

    feat: plain-text emails use the company Site Name

---
+++ resources/views/vendor/mail/text/message.blade.php
@@ -0,0 +1,31 @@
+{{-- The plain-text version's frame, with the company's Site Name. --}}
+@php
+$companyName = \App\Services\Mail\MailLogo::companyName();
+@endphp
+<x-mail::layout>
+    {{-- Header --}}
+    <x-slot:header>
+        <x-mail::header :url="config('app.url')">
+            {{ $companyName }}
+        </x-mail::header>
+    </x-slot:header>
+
+    {{-- Body --}}
+    {{ $slot }}
+
+    {{-- Subcopy --}}
+    @isset($subcopy)
+        <x-slot:subcopy>
+            <x-mail::subcopy>
+                {{ $subcopy }}
+            </x-mail::subcopy>
+        </x-slot:subcopy>
+    @endisset
+
+    {{-- Footer --}}
+    <x-slot:footer>
+        <x-mail::footer>
+            © {{ date('Y') }} {{ $companyName }}. @lang('All rights reserved.')
+        </x-mail::footer>
+    </x-slot:footer>
+</x-mail::layout>
